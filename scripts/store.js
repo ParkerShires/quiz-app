17e2c95cac49147537204568fb02b33ac4215fb2
Parker Shires <parkershires@gmail.com>
2018-05-02 16:03:19 -0400

    should have some functionality

---
+++ scripts/store.js
@@ -0,0 +1,152 @@
+const STORE = [
+    {
+        question: 'Which of the following mythological creatures breathes fire?',
+        answers: [
+            'A: Unicorn',
+            'B: Centaur',
+            'C: Dragon',
+            'D: High-school principal'
+        ],
+        correctAnswer: 'C: Dragon',
+    },
+    {
+        question: 'Which of these phrases is often used to describe a trusted aide?',
+        answers: [
+            'A: Right-hand man',
+            'B: Left-leg lady',
+            'C: Torso buddy',
+            'D: Left-eye lopes'
+        ],
+        correctAnswer: 'A: Right-hand man',
+    },
+    {
+        question: 'A sauna is a room that people generally visit to do what?',
+        answers: [
+            'A: Sleep',
+            'B: Sweat',
+            'C: Eat',
+            'D: Fall in love'
+        ],
+        correctAnswer: 'B: Sweat',
+    },
+    {
+        question: 'In medieval times, which of these professionals was responsible for entertaining the royal courts?',
+        answers: [
+            'A: Barber',
+            'B: Knight',
+            'C: Jester',
+            'D: Blacksmith'
+        ],
+        correctAnswer: 'C: Jester',
+    },
+    {
+        question: 'For over 50 years, what treat was traditionally included in packs of baseball trading cards?',
+        answers: [
+            'A: Chocolate wafer',
+            'B: Gummi bear',
+            'C: Mint stick',
+            'D: Bubblegum'
+        ],
+        correctAnswer: 'D: Bubblegum',
+    },
+    {
+        question: 'A parasol is a lightweight form of what accessory?',
+        answers: [
+            'A: Umbrella',
+            'B: Hat',
+            'C: Cane',
+            'D: Bracelet'
+        ],
+        correctAnswer: 'A: Umbrella',
+    },
+    {
+        question: 'Seaweed is a form of what?',
+        answers: [
+            'A: Bacteria',
+            'B: Algae',
+            'C: Fungus',
+            'D: Sea urchin'
+        ],
+        correctAnswer: 'B: Algae',
+    },
+    {
+        question: 'Who was the leader of the band on the TV series Sha Na Na?',
+        answers: [
+            'A: Benson',
+            'B: Bosley',
+            'C: Bowzer',
+            'D: Baba-Booey'
+        ],
+        correctAnswer: 'C: Bowzer',
+    },
+    {
+        question: 'At the time of his election, who was the oldest person to become President of the United States?',
+        answers: [
+            'A: George Washington',
+            'B: Herbert Hoover',
+            'C: Woodrow Wilson',
+            'D: Ronald Reagan'
+        ],
+        correctAnswer: 'D: Ronald Reagan',
+    },
+    {
+        question: 'The Pecos River is a major tributary of what U.S. river?',
+        answers: [
+            'A: Rio Grande',
+            'B: Platte',
+            'C: Snake',
+            'D: Colorado'
+        ],
+        correctAnswer: 'A: Rio Grande',
+    },
+    {
+        question: `In the 2000 movie Gladiator, Russell Crowe's character is a gladiator under what Roman emperor?`,
+        answers: [
+            'A: Titus',
+            'B: Commodus',
+            'C: Nero',
+            'D: Claudius'
+        ],
+        correctAnswer: 'B: Commodus',
+    },
+    {
+        question: 'In 1911, what did explorer Hiram Bingham discover between two peaks in the Andes mountains?',
+        answers: [
+            'A: City of Machu Picchu',
+            'B: Frozen woolly mammoth',
+            'C: Lake Titicaca',
+            'D: Meteorite from Mars'
+        ],
+        correctAnswer: 'A: City of Machu Picchu',
+    },
+    {
+        question: `What 1953 novel marked the first appearance of Ian Fleming's agent James Bond?`,
+        answers: [
+            'A: Doctor No',
+            'B: Goldfinger',
+            'C: Casino Royale',
+            'D: Live and Let Die '           
+        ],
+        correctAnswer: 'C: Casino Royale',
+    },
+    {
+        question: 'When three celestial bodies form a straight line, what is it called?',
+        answers: [
+            'A: Simoom',
+            'B: Syzygy',
+            'C: String theory',
+            'D: Saprobity'
+        ],
+        correctAnswer: 'B: Syzygy',
+    },
+    {
+        question: 'What letter must appear at the beginning of the registration number of all non-military aircraft in the U.S.?',
+        answers: [
+            'A: N',
+            'B: A',
+            'C: U',
+            'D: L'           
+        ],
+        correctAnswer: 'A: N',
+    }
+];
